ea0fbf2ef4a0effd6b6251bbc6daba68f363bc87
Abdullah Amjad <shkabdullah268@gmail.com>
2026-06-27 06:16:55 -0700

    feat: making the hello-docker container and changing the files on docker runtime but the change was not the permanent

---
+++ hello-docker/index.js
@@ -0,0 +1,8 @@
+console.log("Assalam u alaikum Docker")
+
+
+// docker build .
+// docker run <containerName>
+// docker run -it hello-docker sh
+// You can use the shell to change the files while docker container runtime 
+// but they will only change for that runtime.
